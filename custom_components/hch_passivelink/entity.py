4548da4f2983faea287fe16b37649dc14256e70a
Donnii <donnii@users.noreply.github.com>
2026-08-26 16:59:03 +0200

    Group indoor climate entities on child device

--- custom_components/hch_passivelink/entity.py
+++ custom_components/hch_passivelink/entity.py
@@ -14,6 +14,12 @@ AFTERHEAT_KEYS = {
     "afterheat_temperature",
     "afterheat_raw",
 }
+INDOOR_CLIMATE_KEYS = {
+    "co2",
+    "room_temperature",
+    "relative_humidity",
+    "air_quality",
+}
 
 
 class PassiveLinkEntity(CoordinatorEntity[PassiveLinkCoordinator]):
@@ -23,7 +29,15 @@ class PassiveLinkEntity(CoordinatorEntity[PassiveLinkCoordinator]):
         super().__init__(coordinator)
         self.key = key
         self._attr_unique_id = f"hch5_mk1_hac1_{key}"
-        if key in AFTERHEAT_KEYS:
+        if key in INDOOR_CLIMATE_KEYS:
+            self._attr_device_info = DeviceInfo(
+                identifiers={(DOMAIN, "hch5_mk1_hac1_indoor_climate")},
+                name="Indeklima",
+                manufacturer="Dantherm",
+                model="HAC1 indeklimasensor",
+                via_device=(DOMAIN, "hch5_mk1_hac1"),
+            )
+        elif key in AFTERHEAT_KEYS:
             self._attr_device_info = DeviceInfo(
                 identifiers={(DOMAIN, "hch5_mk1_hac1_afterheat")},
                 name="Eftervarme",
